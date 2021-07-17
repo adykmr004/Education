d733c8e30a59158e25a161bd1db4949038585f7d
adykmr004 <adykmr004@gmail.com>
2021-07-17 17:14:42 +0530

    Updates

---
+++ precache-manifest.93cafe813965b38a4b8fd987c1894bd7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb0c2df95dfcf3cea11650dfb6b7781",
+    "url": "/Education/index.html"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/Education/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "3ec0559afc249cf7ab88",
+    "url": "/Education/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/Education/static/js/2.670fdb07.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Education/static/js/2.670fdb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec0559afc249cf7ab88",
+    "url": "/Education/static/js/main.e7f6c73e.chunk.js"
+  },
+  {
+    "revision": "820a52354d46c456b104",
+    "url": "/Education/static/js/runtime-main.87110cce.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/Education/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/Education/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/Education/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/Education/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/Education/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/Education/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/Education/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/Education/static/media/software.43049cae.jpeg"
+  }
+]);
